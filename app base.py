db397b582c40c8f9a70ccdea12b57a9f09442f61
Daniel Gil <42021676+srd4@users.noreply.github.com>
2019-01-30 16:37:34 -0500

    Update app base.py

--- app base.py
+++ app base.py
@@ -3,18 +3,17 @@ import vlc
 import threading
 from queue import Queue
 
-ms = vlc.MediaPlayer("c:/Users/Pc/Desktop/Daniel/Música/Asap Rocky- Ms ft Lil Wayne.mp3")
-#nota: Es mejor tener una sola funcion para las series, y usar correctamente las variables para que se descanse según el ejercicio.
+ms = vlc.MediaPlayer("c:/Users/Pc/Desktop/Daniel/Música/Asap Rocky- Ms ft Lil Wayne.mp3")#the song to reproduce when countdown is over
 conteo = 0
 
-class Data_tiemptotal (threading.Thread):
+class Data_tiemptotal (threading.Thread):# Takes time from the beginning of the program, to prompt at the end how much the session lasts
     def run(self):
         global conteo
         while True:
             time.sleep(1)
             conteo += 1
 
-def actualizar(a):
+def actualizar(a):#At the end of the session opens/creates a file, with the name of the exercise and writes in it a line with data from the session.
     global ex_sheet
     global conteoseries
     tiempo = "%.2f" % float(conteo/60)
@@ -30,7 +29,7 @@ def actualizar(a):
     f.close
 
 c = 0
-def conteoreps(n):
+def conteoreps(n):#counts reps for the function actualizar()
     global c
     c = c+n
     return str(c)
@@ -45,12 +44,12 @@ def serie():
     def tempo_func():
         
         global reps
-        reps = int(input("¿Cuantas repeticiones hizo? "))
+        reps = int(input("How many reps did you do? "))
         global ejercicio
         conteoreps(reps)
         segundos = int(reps) * ejercicio
         minutos = segundos/60
-        print("Descanso de " + "%.2f" % float(str(minutos)) + " minutos")
+        print("Rest of " + "%.2f" % float(str(minutos)) + " minutes")
 
         for i in range(segundos):
             print("%.2f" % float(segundos/60))
@@ -62,18 +61,18 @@ def serie():
             segundos = segundos - 1
             time.sleep(1)
     tempo_func()
-    seguir = input("¿Siguiente serie? ")
-    if seguir == "si":
-        print("Haga la serie " + str(cuenta))
+    seguir = input("Next set? ")
+    if seguir == "si" or seguir =="yes":
+        print("Make the " + str(cuenta) + " set.")
         ms.stop()
         serie()
     elif seguir == "no":
-        print("Finalizado en " + "%.2f" % float(conteo/60) + " minutos.")
+        print("Finished in " + "%.2f" % float(conteo/60) + " minutes.")
         actualizar(cuenta)
         ms.stop()
         return 0
-    elif seguir == "otro":
-        print("Otro ejercicio")
+    elif seguir == "otro" or seguir == "another":
+        print("Another exercise")
         print("%.2f" % float(conteo/60))
         ms.stop()
         start()
@@ -82,7 +81,7 @@ e = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 desca = [12,34,40,86,35,41,13,17,86]
 
 
-def identifi(n):
+def identifi(n):#to know how to name the file when writing the data at the end of the session
     global ex_sheet
     if n == "1":
         ex_sheet = "Push ups.csv"
@@ -111,20 +110,20 @@ def start():
     global desca
     global ejercicio
     que_hacer = input("\nPush ups = 1\nHandstand push ups = 2\nHollow body position crunches "
-                  "= 3\nFront progressions = 4\nPull ups = 5\nPseudo-planche push ups = 6\nDips = 7\nLumbar holds = 8\nFull planche progressions = 9\n\n¿Que va a hacer? ")
+                  "= 3\nFront progressions = 4\nPull ups = 5\nPseudo-planche push ups = 6\nDips = 7\nLumbar holds = 8\nFull planche progressions = 9\n\nWhat do you want to do? ")
 
 
     if int(que_hacer) in range(1,10):
-        cuenta = int(input("¿Que serie va a hacer (número)? "))
+        cuenta = int(input("What set are you going to do (number)? "))
         if int(cuenta) >= 0:
-            print("Puede empezar...")
+            print("You can start...")
         for i in e:
             if que_hacer == i:
                 identifi(i)
                 ejercicio = desca[e.index(i)]
                 serie()
     else:
-        print("Inválido")
+        print("Unvalid")
         start()
 
 start()
